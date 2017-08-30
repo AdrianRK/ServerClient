e5295955a818a56f5fb10a537dbf8313c4294216
Adrian Alin Radu <adrian.alin.radu@protonmail.com>
2017-08-30 12:02:11 +0000

    Finished socket wrapper: Start test;

--- src/CWSocket.cpp
+++ src/CWSocket.cpp
@@ -82,27 +82,78 @@ CWsocket::CWsocket (int sockif): mSocket(sockif)
 CWsocket::~CWsocket ()
 {
 	close(mSocket);
-}		/* -----  end of method CWsocket::~CWSocket (destructor) ----- */
+}		/* -----  end of method CWsocket::~CWsocket (destructor) ----- */
 
-
-	CWsocket
-CWsocket::Accept ()
-{
-	return *this;
-}		/* -----  end of method CWsocket::Accept  ----- */
-
-
-int CWSocket::Bind(const std::string&, int port)
+int CWsocket::Bind(const std::string& ip, int port)
 {
 	struct sockaddr_in addr; 
 	addr.sin_family = AF_INET6;
 	addr.sin_addr.s_addr = inet_addr(ip.c_str()); 
 	addr.sin_port = htons(port);
-	ret = bind(mSocket,reinterpret_cast<sockaddr*>(&addr),sizeof(addr));
+	int ret = bind(mSocket,reinterpret_cast<sockaddr*>(&addr),sizeof(addr));
 	if	(-1 == ret)
 	{
 		std::cerr<< "Cannor create socket!\n";
 		exit(1);
 	}
+	return ret;
 }
 
+int CWsocket::Connect(const std::string& ip, int port)
+{
+	struct sockaddr_in addr; 
+	addr.sin_family = AF_INET6;
+	addr.sin_addr.s_addr = inet_addr(ip.c_str()); 
+	addr.sin_port = htons(port);
+	int ret = bind(mSocket,reinterpret_cast<sockaddr*>(&addr),sizeof(addr));
+	if	(-1 == ret)
+	{
+		std::cerr<< "Cannot Connect to socket!\n";
+		exit(1);
+	}
+	return ret;
+}
+
+
+	int
+CWsocket::Listen ( )
+{
+	int ret = listen(mSocket,10);
+	
+	if (-1 == ret) 
+	{
+		std::cerr << "Error listening on socket!\n";
+	}
+	return ret;
+}		/* -----  end of method CWsocket::Listen  ----- */
+
+
+
+
+	CWsocket
+CWsocket::Accept ( )
+{
+	int ret = accept(mSocket, nullptr, nullptr);
+	return CWsocket(ret);
+}		/* -----  end of method CWsocket::Accept  ----- */
+
+
+
+int CWsocket::Send(const std::vector<unsigned char>& buffer)
+{
+	int ret = send(mSocket, buffer.data(), buffer.size(), 0);
+	if (-1 == ret) 
+	{
+		std::cerr << "Error listening on socket!\n";
+	}
+	return ret;
+}
+
+
+const std::vector<unsigned char> CWsocket::Receive()
+{
+	unsigned char buffer[256] = {0,};
+	int ret = recv(mSocket, buffer, sizeof(unsigned char) * 256, 0);
+	std::vector<unsigned char> buff(buffer, buffer+ret);
+	return buff;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include "CWSocket.hpp"
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -33,10 +34,17 @@ main ( int argc, char *argv[] )
 		if (std::string(argv[1]) == "Server" || std::string(argv[1]) == "server")
 		{
 			std::cout<< "Start Server";	
+			CWsocket server("127.0.0.1",8000);
+			if (server.Listen())
+			{
+				CWsocket newConn(server.Accept());
+			}
 		}
 		if (std::string(argv[1]) == "Client" || std::string(argv[1]) == "client")
 		{
 			std::cout<< "Start Client";	
+			CWsocket client;
+			client.Connect("127.0.0.1",800);
 		}
 	}
 	return EXIT_SUCCESS;
